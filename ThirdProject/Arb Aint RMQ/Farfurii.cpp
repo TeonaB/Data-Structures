b1f101dbf3bd48484e4995bf563bff561ffc8a47
TEONA-CHRISTIANA BOBIC <99075325+TeonaB@users.noreply.github.com>
2023-05-28 23:55:23 +0300

    Update Farfurii.cpp

--- ThirdProject/Arb Aint RMQ/Farfurii.cpp
+++ ThirdProject/Arb Aint RMQ/Farfurii.cpp
@@ -10,16 +10,17 @@ int main()
     long N, K;
     f>>N>>K;
     long  i=1;
+    ///atata timp cat mai putem inca crea perechi de farfurii cu marimea i afisam
     while((N-i) * (N-i-1) /2 > K)
     {
         g<<i<<" ";
         i++;
     }
-
+    ///calculam pentru restul de marimi de farfurii diferents
     long  dif = K - ((N-i) * (N-i-1)/2);
 
     g<<i+dif<<' ';
-
+    ///si afisam restul [erechilor
     for(long j =N;j>=i;j--)
         if(j!=i+dif)
             g<<j<<" ";
