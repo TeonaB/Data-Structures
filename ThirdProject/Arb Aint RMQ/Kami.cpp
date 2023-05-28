3d49495d4a0df6acdd2340a224135ff6aaf94747
TEONA-CHRISTIANA BOBIC <99075325+TeonaB@users.noreply.github.com>
2023-05-29 00:00:39 +0300

    Update Kami.cpp

--- ThirdProject/Arb Aint RMQ/Kami.cpp
+++ ThirdProject/Arb Aint RMQ/Kami.cpp
@@ -1,48 +1,57 @@
 #include <fstream>
 #include <iostream>
 using namespace std;
-ifstream f("kami.in");
-ofstream g("kami.out");
-int N,M,z[100001],sum,maxim;
+
+ifstream f("kami.in");  
+ofstream g("kami.out");  
+
+int N, M, z[100001], sum, maxim;  
+
 void zapada(int a)
 {
-    sum=0;
-    sum += z[a];
-    a--;
-    while(sum > z[a] && sum<=maxim)
+    sum = 0;
+    sum += z[a];  // Adaugam valoarea din nivelul z[a] la suma
+    a--;  
+    while (sum > z[a] && sum <= maxim)
     {
-        sum += z[a];
-        a--;
+        sum += z[a];  
+        a--;  // Coboram ca si nivel
     }
-    if(sum>maxim)
-        g<<0<<'\n';
+    if (sum > maxim)
+        g << 0 << '\n';  // Daca ajungem la baza
     else
-        g<<a<<'\n';
+        g << a << '\n';  // Atfel va fi o afisare normala
 }
+
 int main()
-{   z[0]=10000000000;
-    maxim=0;
-    f>>N;
-    for(int i=1; i<=N; i++)
-        {f>>z[i];
-        maxim=max(maxim,z[i]);}
-    f>>M;
-    for(int i=1; i<=M; i++)
+{
+    z[0] = 10000000000;  // Initializam nivelul z[0] cu o valoare mare
+    maxim = 0;  // Initializam variabila maxim cu 0
+
+    f >> N;  
+    for (int i = 1; i <= N; i++)
     {
-        int x,y,a,b;
-        f>>x;
-        if(x==1)
+        f >> z[i];  
+        maxim = max(maxim, z[i]);  // Actualizam valoarea maxim
+    }
+    ///conform cerintei, in functi de ce x citim efectuam operatiile
+    f >> M;  
+    for (int i = 1; i <= M; i++)
+    {
+        int x, y, a, b;
+        f >> x;  
+        if (x == 1)
         {
-            f>>y;
-            zapada(y);
+            f >> y; 
+            zapada(y);  
         }
         else
         {
-            f>>a>>b;
-            z[a]=b;
-            maxim=max(maxim,z[i]);
+            f >> a >> b;  
+            z[a] = b;  // Actualizam valoarea nivelului z[a] cu b
+            maxim = max(maxim, z[i]);  // Actualizam valoarea maxim
         }
     }
-    return 0;
 
+    return 0;  
 }
