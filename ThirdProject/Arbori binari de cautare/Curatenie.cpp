5f2f6bc0f36ed318cb696a792de74bef27de3871
TEONA-CHRISTIANA BOBIC <99075325+TeonaB@users.noreply.github.com>
2023-05-29 00:28:35 +0300

    Update Curatenie.cpp

--- ThirdProject/Arbori binari de cautare/Curatenie.cpp
+++ ThirdProject/Arbori binari de cautare/Curatenie.cpp
@@ -3,15 +3,16 @@ using namespace std;
 ifstream f("curatenie.in");
 ofstream g("curatenie.out");
 int n, v[500001], stg[500000], dr[500000];
+///avem un interval 1 j de persoane de transformari
 int curatenie(int i, int j)
-    {   if (i>j)
+    {   if (i>j) ///verificam sa fie valabil intervalul
             return 0;
         int aux;
         f>>aux;
-        stg[aux] = curatenie(i, v[aux]-1);
+        stg[aux] = curatenie(i, v[aux]-1);  //calculam curatenia pt fiecare interval posibil
         dr[aux] = curatenie(v[aux]+1, j);
 
-        return aux;
+        return aux; //returneaza persoana pentru curatenie
     }
 int main()
 {
@@ -22,6 +23,7 @@ int main()
             f>>x;
             v[x] = i;
         }
+    //aplicam pentru tot intervalul si afisam intervalele
     curatenie(0, n - 1);
 
     for(int i=1; i<=n; i++)
