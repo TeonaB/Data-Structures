6a406524b68358b5069c5e78b2dbe42e31257a8b
TEONA-CHRISTIANA BOBIC <99075325+TeonaB@users.noreply.github.com>
2023-05-29 01:25:32 +0300

    Update Paranteze.cpp

--- ThirdProject/Stive-Liste-Cozi/Paranteze.cpp
+++ ThirdProject/Stive-Liste-Cozi/Paranteze.cpp
@@ -6,15 +6,15 @@ using namespace std;
 ifstream f("paranteze.in");
 ofstream g("paranteze.out");
 int max_paranteze(string sir, int n)
-{
+{   ///folosim stack pentru a putea vedea top-ul usor
     stack <int> s;
-    int v[n];
+    int v[n]; ///cu acest vector marcam parantezele corecte
     for(int i=0; i<n; i++)
-    {
+    {   ///verificam sa incepem cu o paranteza deschisa si adaugam in stiva 
         if(sir[i]=='('||sir[i]=='['||sir[i]=='{')
                 s.push(i);
-        else
-            if (!s.empty())
+        else ///daca nu incepem cu paranteza ori avem stiva goala si adaugam indicele 
+            if (!s.empty()) ///ori verificam daca parantezele si secventa se vor inchide bine in capete si atunci le marcam cu 1 in vector
                 if((sir[s.top()] == '{' && sir[i]=='}')||(sir[s.top()] == '[' && sir[i]==']')||(sir[s.top()] == '(' && sir[i]==')'))
                 {
                     v[i] = 1;
